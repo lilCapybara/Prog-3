b046d39388046fa999867f5230c04bac89dbcd65
Sebastian Ortega <142858679+lilCapybara@users.noreply.github.com>
2024-04-17 13:03:16 -0300

    Add files via upload

--- TP2/Main.java
+++ TP2/Main.java
@@ -6,19 +6,54 @@ public class Main {
 
     }*/
 
-    public static String decimalABinario(int num,String resultado){
+    public static String convertirABinario(int num){
 
-        if(num/2!=1){
-            decimalABinario(num/2,);
+
+        if(num<0){
+            return "-1";
+
+        }else if(num==1){
+            return "1";
+
+        }else{
+            return convertirABinario(num / 2) + String.valueOf(num % 2);
         }
-        return resultado;
     }
+
+    public static int fibonacci (int n){    //Obtiene el termino n de la sucesion de fibonacci
+        if(n==0 || n==1){
+            return n;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+
+    public static void fibonacciHelper(int n){ //Realiza el recorrido para la sucesion de fibonacci
+        for (int i=0; i<n; i++){
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    public static void valorIgualIndice(int[]a,int i){
+        if(a[i]==i){
+            System.out.println("El indice y su valor son iguales para i="+i);
+        }
+        if(i<a.length-1) {
+            valorIgualIndice(a,i+1);
+        }
+    }
+
 
     public static void main(String[] args) {
         /*int[]arreglo={1,2,3,4,5};
         OrenadorDeArreglos ordenador=new OrenadorDeArreglos(arreglo);
         ordenador.verificarOrdenado(0);*/
 
-        decimalABinario(26,"");
+        //System.out.println(convertirABinario(1));
+
+        //fibonacciHelper(6);
+
+        /*int[]a={0,1,5,3,4,5};
+        valorIgualIndice(a,0);*/
     }
 }
