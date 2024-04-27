480a49322a0adbc97b3ed4f4433d87c3feb32da2
Sebastian Ortega <142858679+lilCapybara@users.noreply.github.com>
2024-04-27 12:34:57 -0300

    Agregada busqueda binaria recursiva

--- TP2/Main.java
+++ TP2/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static String convertirABinario(int num){
 
+
         if(num<0){
             return "-1";
 
@@ -20,43 +21,62 @@ public class Main {
     }
 
     public static int fibonacci (int n){    //Obtiene el termino n de la sucesion de fibonacci
-        if(n==1){
-            return 0;
-        }else if(n==2){
-            return 1;
+        if(n==0 || n==1){
+            return n;
         }else{
             return fibonacci(n-1)+fibonacci(n-2);
         }
     }
 
     public static void fibonacciHelper(int n){ //Realiza el recorrido para la sucesion de fibonacci
-        for (int i=1; i<=n; i++){
+        for (int i=0; i<n; i++){
             System.out.println(fibonacci(i));
         }
     }
 
-    public static int valorIgualIndice(int[]a,int i){
+    public static void valorIgualIndice(int[]a,int i){
         if(a[i]==i){
-            return i;
+            System.out.println("El indice y su valor son iguales para i="+i);
         }
-        if(i<a.length-1) {  //Si lo hay, pasa al siguiente valor del arreglo
-            return valorIgualIndice(a,i+1);
+        if(i<a.length-1) {
+            valorIgualIndice(a,i+1);
         }
-        return -1;
     }
 
+    public static int busquedaBinaria(int[]a,int x){    //Metodo usado por el usuario en el main
+
+    }
+
+    public static int busquedaBinaria(int[]a, int x, int inicio, int fin){  //Metodo que realiza la busqueda binaria
+        int medio;
+        if(fin<inicio){ //Esto ocurrira cuando se intente aplicar el metodo recursivamente a un subarreglo de 1 elemento
+            return -1;
+        }else{
+            medio=(inicio+fin)/2;
+            if(x>a[medio]){ //Si el valor buscado es mayor al valor de la mitad del arreglo
+                return busquedaBinaria(a,x,medio+1,fin);    //Aplico la busqueda binaria en la mitad derecha
+
+            } else if (x<a[medio]) {    //Si el valor buscado es menor al valor de la mitad del arreglo
+                return busquedaBinaria(a,x,inicio,medio-1); //Aplico la busqueda binaria en la mitad izquierda
+
+            }else{
+                return medio;   //Si el valor no es ni mayor ni menor al de la mitad del arreglo, es igual, por lo que se encontro el valor buscado
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        /*int[]arreglo={1,2,3,4,5};
-        OrenadorDeArreglos ordenador=new OrenadorDeArreglos(arreglo);
+        int[]arreglo={1,2,3,4,5,8,14};
+        /* OrenadorDeArreglos ordenador=new OrenadorDeArreglos(arreglo);
         ordenador.verificarOrdenado(0);*/
 
         //System.out.println(convertirABinario(1));
 
         //fibonacciHelper(6);
-        //System.out.println(fibonacci(6));
 
-        int[]a={6,3,1,1,1,1};
-        System.out.println(valorIgualIndice(a,0));
-
+        /*int[]a={0,1,5,3,4,5};
+        valorIgualIndice(a,0);*/
+        busquedaBinaria(arreglo,5);
     }
 }
